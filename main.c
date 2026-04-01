3b34713f801fbc37ed19549b09673c61cfd992d3
gaurav00008 <gauravpaneru95@gmail.com>
2026-04-01 21:02:43 +0545

    TO ADD TWO NUMBERS

--- main.c
+++ main.c
@@ -1,7 +1,21 @@
+// #include <stdio.h>
+
+// int main() {
+//     // Print "Hello, World!" to the console
+//     printf("Hello, World!\n");
+//     return 0;
+// }
+
+
+//TO ADD TWO NUMBERS
+
 #include <stdio.h>
 
 int main() {
-    // Print "Hello, World!" to the console
-    printf("Hello, World!\n");
+    int a = 5, b = 10, sum;
+
+    sum = a + b;
+
+    printf("Sum = %d\n", sum);
     return 0;
 }

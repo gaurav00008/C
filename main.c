3d14a1e4586ab48a41567334ea702cda4044f1d4
gaurav00008 <gauravpaneru95@gmail.com>
2026-04-01 21:04:17 +0545

    TO CHECK WHETER THE NUMBER IS EVEN OR ODD

--- main.c
+++ main.c
@@ -9,13 +9,32 @@
 
 //TO ADD TWO NUMBERS
 
-#include <stdio.h>
+// #include <stdio.h>
 
-int main() {
-    int a = 5, b = 10, sum;
+// int main() {
+//     int a = 5, b = 10, sum;
 
-    sum = a + b;
+//     sum = a + b;
 
-    printf("Sum = %d\n", sum);
-    return 0;
-}
+//     printf("Sum = %d\n", sum);
+//     return 0;
+// }
+
+
+
+
+/*TO CHECK EVEN OR ODD*/
+
+// #include <stdio.h>
+
+// int main() {
+//     int num = 7;
+
+//     if (num % 2 == 0) {
+//         printf("Even\n");
+//     } else {
+//         printf("Odd\n");
+//     }
+
+//     return 0;
+// }
